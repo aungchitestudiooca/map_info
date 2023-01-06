70cb6bb4b0aebf8aba3c840c56d6603b695601bc
aungchitestudiooca <aungchit@estudiooca.com>
2023-01-06 17:43:11 +0700

    upload

---
+++ data/Analysis_Boundary_Eco_3.js
@@ -0,0 +1,8 @@
+var json_Analysis_Boundary_Eco_3 = {
+"type": "FeatureCollection",
+"name": "Analysis_Boundary_Eco_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 102.608314166359321, 17.286918307389641 ], [ 102.608314166359165, 17.656235645141702 ], [ 102.999772677742683, 17.656235645141798 ], [ 102.999772677742726, 17.286918307389747 ], [ 102.608314166359321, 17.286918307389641 ] ] ] ] } }
+]
+}
